3c52cf91499c9e3c43bd78e2824d9b69aef37929
per5cm <per5cm@proton.me>
2025-08-21 13:25:27 +0200

    bump

--- CSharpMP/src/Loops/Program.cs
+++ CSharpMP/src/Loops/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int zahl = 21;
+// int i;
+
+
+// void Begruss(string name, int alter, bool volljaerig, double gewicht)
+// {
+//     Console.WriteLine("Hallo " + name + " Alter: " + alter + volljaerig + gewicht);
+// }
+
+// Begruss("Erikas", 37, false, 81.3);
+
+
+// Primzahlen berechnen 1.0
+
+
+int Input;
+bool IsPrime(int zahl)
+{
+    bool teilbar = false;
+    decimal grenze = (decimal)Math.Sqrt(zahl);
+    
+    for (int i = 2; i < grenze; i++)
+    {
+        if (zahl % i == 0)
+        {
+            Console.WriteLine("Teilbar durch " + i);
+            teilbar = true;
+        }
+    }
+    return !teilbar;
+}
+
+
+Console.WriteLine("Primzahl-Test");
+// Console.Write("Gebe ein Zahl zum testen ein: ");
+do
+{
+    Console.Write("Gebe eine Zahl zum testen ein (0 = Beenden): ");
+
+    if (int.TryParse(Console.ReadLine(), out Input))
+    {
+        if (IsPrime(Input))
+        {
+            Console.WriteLine(Input + " ist eine Primzahl");
+        }
+        else
+        {
+            Console.WriteLine(Input + " ist keine Primzahl");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Uengultige Eingabe");
+    }
+} while (Input != 0);    
--- CSharpMP/src/Test/Program.cs
+++ CSharpMP/src/Test/Program.cs
@@ -1,52 +1,62 @@
-﻿Console.WriteLine("\nI love Pizza!");
-Console.WriteLine("Musterstrasse 1");
-Console.WriteLine("1234 Musterstadt\n");
+﻿
+Console.WriteLine("Einsteins - E = mc^2");
+Console.Write("Input mass: ");
+int mass = int.Parse(Console.ReadLine());
+long speed_of_light = 299792458;
+long Energy = mass * speed_of_light * speed_of_light;
 
-Console.WriteLine("Da hat wohl\nirgendjemand in\n\tWord ein\n\nBild zu viel\nverschoben!\n");
+Console.WriteLine("Energy: " + Energy);
 
-Console.WriteLine("System.out.println(\"Sie sind ein Künstler, wenn dieser Text erscheint;\" \\(^.^)/\n");
 
-double x = 1.0;
-double y = 2.5;
+// Console.WriteLine("\nI love Pizza!");
+// Console.WriteLine("Musterstrasse 1");
+// Console.WriteLine("1234 Musterstadt\n");
 
-Console.WriteLine("x / y = " + x/y);
-Console.WriteLine(x + y);
-Console.WriteLine(1 + 2 + 3 + 4);
-Console.WriteLine(1 + (2 + 3) + 4);
-Console.WriteLine(1 + 2+ 3 + "4");
-Console.WriteLine("1" + 2 + 3 + 4);
-Console.WriteLine("Hilfe " + true + 3 + "\n");
+// Console.WriteLine("Da hat wohl\nirgendjemand in\n\tWord ein\n\nBild zu viel\nverschoben!\n");
+
+// Console.WriteLine("System.out.println(\"Sie sind ein Künstler, wenn dieser Text erscheint;\" \\(^.^)/\n");
+
+// double x = 1.0;
+// double y = 2.5;
+
+// Console.WriteLine("x / y = " + x/y);
+// Console.WriteLine(x + y);
+// Console.WriteLine(1 + 2 + 3 + 4);
+// Console.WriteLine(1 + (2 + 3) + 4);
+// Console.WriteLine(1 + 2+ 3 + "4");
+// Console.WriteLine("1" + 2 + 3 + 4);
+// Console.WriteLine("Hilfe " + true + 3 + "\n");
 //Console.WriteLine(true + 3 + "Hilfe"); <--- fehler ausgibt.
 
-int a = 1;
-int b = 2;
-int c = 3;
-int d = 4;
+// int a = 1;
+// int b = 2;
+// int c = 3;
+// int d = 4;
 
-Console.WriteLine(a++);
-Console.WriteLine(a);
-Console.WriteLine(++b);
-Console.WriteLine(b);
-Console.WriteLine((++c)+(c++));
-Console.WriteLine(c + "\n");
+// Console.WriteLine(a++);
+// Console.WriteLine(a);
+// Console.WriteLine(++b);
+// Console.WriteLine(b);
+// Console.WriteLine((++c)+(c++));
+// Console.WriteLine(c + "\n");
 
-int nr = 7;
+// int nr = 7;
 
-Console.WriteLine(nr < 9 && nr >= -5);
-Console.WriteLine(nr != 7 && nr >= 3);
-Console.WriteLine(nr++ == 8 || nr == 7);
-Console.WriteLine((nr > 0 && nr < 10) || nr != 7);
-Console.WriteLine((nr < 0 || nr > 5) && (nr <= 7 || nr > 10));
-Console.WriteLine("\n");
+// Console.WriteLine(nr < 9 && nr >= -5);
+// Console.WriteLine(nr != 7 && nr >= 3);
+// Console.WriteLine(nr++ == 8 || nr == 7);
+// Console.WriteLine((nr > 0 && nr < 10) || nr != 7);
+// Console.WriteLine((nr < 0 || nr > 5) && (nr <= 7 || nr > 10));
+// Console.WriteLine("\n");
 
-int i = -3;
+// int i = -3;
 
-Console.WriteLine((6 * ++i)-8 % 3);
-Console.WriteLine(i);
+// Console.WriteLine((6 * ++i)-8 % 3);
+// Console.WriteLine(i);
 
-i = 7;
+// i = 7;
 
-Console.WriteLine((i % 3 == 1) && (i >= 5));
-Console.WriteLine(!(i < 6 || i > 7)); // dreht die loging, z.b. suche den argument in the liste und er hat bestimmten wert nicht gefunden.
-Console.WriteLine((i / 2 == 3 || i == 7) && i != 0);
-Console.WriteLine((i * 2 > 10) && (i + 1 <= 8));
+// Console.WriteLine((i % 3 == 1) && (i >= 5));
+// Console.WriteLine(!(i < 6 || i > 7)); // dreht die loging, z.b. suche den argument in the liste und er hat bestimmten wert nicht gefunden.
+// Console.WriteLine((i / 2 == 3 || i == 7) && i != 0);
+// Console.WriteLine((i * 2 > 10) && (i + 1 <= 8));
