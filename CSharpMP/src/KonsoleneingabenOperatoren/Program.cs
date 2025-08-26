cf5429d429b9a4afc122829aad7f4fda2b9091a9
per5cm <per5cm@proton.me>
2025-08-26 15:48:48 +0200

    dumbp

---
+++ CSharpMP/src/KonsoleneingabenOperatoren/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Rechner\n");
+
+// Console.WriteLine("Bitte geben Sie zwei ganzen Zahle ein!");
+// Console.Write("Zahl 1: ");
+// string input_zahl_1 = Console.ReadLine();
+// int zahl_1 = int.Parse(input_zahl_1);
+// Console.Write("Zahl 2: ");
+// string input_zahl_2 = Console.ReadLine();
+// int zahl_2 = int.Parse(input_zahl_2);
+
+
+// Console.WriteLine($"Summe: {zahl_1 + zahl_2}");
+// Console.WriteLine($"Differenz: {zahl_1 - zahl_2}");
+// Console.WriteLine($"Produkt: {zahl_1 * zahl_2}");
+// Console.WriteLine($"Quotient: {zahl_1 % zahl_2}");
+
+
+// Console.WriteLine("\nBenzin");
+
+// Console.Write("Bitte geben Sie die gefahreren Kilometer ein: ");
+// int zahl_kilometer = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Bitte geben Sie die verbrauchten Benzinmenge in Litern ein: ");
+// float zahl_benzin = Convert.ToSingle(Console.ReadLine());
+
+// float verbrauch = (zahl_benzin / zahl_kilometer) * 100;
+
+
+// Console.WriteLine($"Der Druschnittsverbrauch ist: {verbrauch} Liter/100km");
+
+
+
+// Console.WriteLine("\nDreiecksberechnung");
+
+// Console.Write("Berechnung Grundlinie: ");
+// float zahl_grundlinie = Convert.ToSingle(Console.ReadLine());
+
+// Console.Write("Berechnung die Höhe: ");
+// float zahl_hoehe = Convert.ToSingle(Console.ReadLine());
+
+// float zahl_dreieckberechnung = (zahl_grundlinie * zahl_hoehe) / 2;
+
+// Console.WriteLine($"Die Fläche beträgt: {zahl_dreieckberechnung}");
+
+
+// Console.WriteLine("Währungsrechner");
+
+// Console.Write("Eingeben Betrag in Euro: ");
+// float zahl_euro = Convert.ToSingle(Console.ReadLine());
+
+// float euro_wert = 1.3f;
+
+// float dollar_wert = zahl_euro * euro_wert;
+
+// Console.WriteLine($"Dollarbetrag: {dollar_wert:F2} $");
+
+
+// Console.WriteLine("\nSchreinereihelfer Bretter");
+
+// Console.Write("Brett Länge in cm eingeben: ");
+// int brett_laenge = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Holzstück Länge in cm eingeben: ");
+// int holstueck_laenge = Convert.ToInt32(Console.ReadLine());
+
+// int holzstueck_menge = brett_laenge / holstueck_laenge;
+
+// Console.WriteLine($"Holzstücke: {holzstueck_menge}");
+
+// double verchnitt = holzstueck_menge * holstueck_laenge;
+// double verschnitt_final = brett_laenge - verchnitt;
+
+// Console.WriteLine($"Verschnitt in cm: {verschnitt_final}");
+
+
---
+++ CSharpMP/src/if_operator/Program.cs
@@ -0,0 +1,55 @@
+﻿// Console.WriteLine("Berechnung Bestellwert");
+// Console.Write("Bestellwert in Euro: ");
+
+// Ausführlich
+//string eingabe = Console.ReadLine();
+//double eingabeUmgewandelt = double.Parse(eingabe);
+
+// 2 Schritte auf einmal
+// double bestellWert  = double.Parse(Console.ReadLine());
+// double ergebnis = bestellWert;
+
+// if(bestellWert < 200)
+// {
+//     ergebnis = ergebnis + 5.5;
+// }
+
+// Console.WriteLine($"Rechnungsbetrag {ergebnis}" );
+// Console.WriteLine("Programmende Bestell");
+
+// Console.WriteLine("Ein Dreieck Berechnung");
+
+// Console.Write("Bitte Seite A eingeben: ");
+// float seite_a = Convert.ToSingle(Console.ReadLine());
+
+// Console.Write("Bitte Seite B eingeben: ");
+// float seite_b = Convert.ToSingle(Console.ReadLine());
+
+// Console.Write("Bitte seite C eingeben: ");
+// float seite_c = Convert.ToSingle(Console.ReadLine());
+
+
+Console.WriteLine("Gleichung X");
+
+Console.Write("Gleichunn A: ");
+int gleichung_a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Gleichung B: ");
+int gleichung_b = Convert.ToInt32(Console.ReadLine());
+
+// Prüfung Fehler bei ungültiger Eingabe a = b
+if (gleichung_a == 0)
+{
+    Console.WriteLine("Fehler: ");
+}
+
+else if (gleichung_b == 0)
+{
+    Console.WriteLine("Fehler: ");
+}
+
+else
+{
+    double ergebnis_x = -gleichung_b / gleichung_a;
+    Console.WriteLine($"Lösung: {ergebnis_x}");
+}
