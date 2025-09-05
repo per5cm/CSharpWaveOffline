f27d17996402026a0e3a14b79dbc3e1b1795e4cd
per5cm <per5cm@proton.me>
2025-09-05 13:50:23 +0200

    bump

---
+++ CSharpMP/src/arrays_list/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Aktiviere genau das, was du testen willst: ");
+        //array_1();
+        //array_2();
+        array_3();
+
+    }
+    #region Array
+
+    static void array_1()
+    {
+        int[] first_array = [3, 5, 7, 9];
+        //Console.WriteLine(first_array[0]);
+
+        foreach (int i in first_array)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+
+    static void array_2()
+    {
+        int[] array_list = { 2, 4, 6, 8 };
+        int summe = 0;
+        int average = 0;
+
+        //for (int items = 0; items < array_list.Length; items++)
+        foreach (var item in array_list)
+        {
+            summe += item;//array_list[items];
+        }
+        average = summe / array_list.Length;
+        Console.WriteLine($"Array average is: {average}."); // for each = summe 
+    }
+
+    static void array_3()
+    {
+        int[] array_random_numbers = { -3, 10, 5, 12, 0 };
+        int max = array_random_numbers[0];
+        int min = array_random_numbers[0];
+
+        for (int item = 0; item < array_random_numbers.Length; item++)
+        {
+            if (array_random_numbers[item] > max)
+            {
+                max = array_random_numbers[item];
+            }
+            if (array_random_numbers[item] < min)
+            {
+                min = array_random_numbers[item];
+            }
+        }
+        Console.WriteLine($"Array maximum number is: {max}.");
+        Console.WriteLine($"Array minimum number is: {min}.");
+    }
+    #endregion
+}
+    
