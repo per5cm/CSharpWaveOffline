695ef7ac3d4b8253259a087a9c028903e3ba745b
per5cm <per5cm@proton.me>
2025-09-11 10:54:12 +0200

    update

--- ProjecktMappe/src/Vpets/Program.cs
+++ ProjecktMappe/src/Vpets/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPets.Models
+{
+    internal class Pet
+    {
+        // Felder des Haustiers (zur Speicherung von Daten)
+        public string Name = "Namenlos";
+        public int Hunger;
+        public int Energy;
+        public int Mood;
+    }
+}
