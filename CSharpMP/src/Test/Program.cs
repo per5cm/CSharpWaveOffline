83f0e113e2803223001c4494c6612cb4386488c4
per5cm <per5cm@proton.me>
2025-09-11 08:32:34 +0200

    update

--- CSharpMP/src/Test/Program.cs
+++ CSharpMP/src/Test/Program.cs
@@ -1,12 +1,41 @@
 ﻿
-Console.WriteLine("Einsteins E = mc^2");
-Console.Write("Input mass in kg: ");
-double m = double.Parse(Console.ReadLine());
+Console.WriteLine("Hello, World!");
 
-const double c = 299792458;
-double E = m * c * c;   // E = m * c^2
+// hier ist komentar /* multi komentare */
 
-Console.WriteLine($"{E:N0} J");
+string name = "Erik";
+int age = 30;
+
+//console.WriteLine($"I love" + name + "and Pizza! He is: " + age + "years old.");
+// $ ist F-string version von Python.
+Console.WriteLine($"I love {name} and Pizza! He is: {age} years old.");
+
+// Kundin Sandra Schmidt (27 Jahre) hat Konto-Nr. 
+// 9876543210 -M Letzte Messung: 23.7°C Pi = 3.14159. Preis netto: 19.95 € Mitglied? True
+
+//Unterstriche nur für die Lesbarkeit! 
+long accountId = 987_654_321_0;
+float temperature = 23.7f;
+double pi = 3.14159;
+decimal price = 19.95m;
+bool isMember = true;
+char separator = '-';
+string firstName = "Sandra";
+string lastName = "Schmidt";
+
+Console.WriteLine($"Kundin {firstName} {lastName} {age} Jahre alt"
++ $"\nKonto-Nr. {accountId}{separator}M Letzte Messung: {temperature}°C"
++ $"Pi = {pi}. Preis netto: {price} € Mitglied: {isMember}");
+
+
+// Console.WriteLine("Einsteins E = mc^2");
+// Console.Write("Input mass in kg: ");
+// double m = double.Parse(Console.ReadLine());
+
+// const double c = 299792458;
+// double E = m * c * c;   // E = m * c^2
+
+// Console.WriteLine($"{E:N0} J");
 
 
 // int zahl = 21;
