1e068a731a5a1359f740794f1d28e42c495e982f
per5cm <per5cm@proton.me>
2025-09-16 11:21:17 +0200

    bump while

---
+++ CSharpMP/src/loops_tasks/Program.cs
@@ -0,0 +1,16 @@
+﻿// Console.WriteLine("Ganze Zahlen");
+
+// Console.Write("Geben Sie eine ganze Zahl ein: ");
+// string input = Console.ReadLine();
+// int number = int.Parse(input);
+
+// int whole_number = 1;
+
+// while (whole_number <= number)
+// {
+//     if (number % whole_number == 0)
+//     Console.WriteLine($"{number} teilbar durch: {whole_number}."); // Write geht auch.
+//     whole_number++;
+// }
+
+Console.WriteLine("Schulnoten");
