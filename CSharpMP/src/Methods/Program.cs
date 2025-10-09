37583d163ffa51993e2d8d73226e7e70079a7b13
per5cm <per5cm@proton.me>
2025-10-09 13:21:54 +0200

    update

---
+++ CSharpMP/src/Methods/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    // Hallo Welt.
+    // static void SagHallo()
+    // {
+    //     Console.WriteLine("Hallo Welt! Ich Liebe Pizza!");
+    // }
+
+    // static void Main(string[] args)
+    // {
+    //     SagHallo();
+    // }
+
+    // Hallo Zu.
+    static void SagHalloZu(string name)
+    {
+        Console.WriteLine("Hallo, " + name);
+    }
+
+    static void Main(string[] args)
+    {
+        SagHalloZu("Maria!");
+        SagHalloZu("Alibaba!");
+        SagHalloZu("Max!");
+    }
+}
