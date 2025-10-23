aa44cca2aa233eb621bd8e9e90cb273047b2fa6c
per5cm <per5cm@proton.me>
2025-10-23 12:28:15 +0200

    fix UI

--- ProjecktMappe/src/MyUI/obj/Debug/net9.0-windows/MyUI.AssemblyInfo.cs
+++ ProjecktMappe/src/MyUI/obj/Debug/net9.0-windows/MyUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24b132c743ff1dfa128cb3cbe12926e45068a57e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84859c62b5e6ccb978fa9355cf3aeccce791d82a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProjecktMappe/src/Vpets/obj/Debug/net9.0/Vpets.AssemblyInfo.cs
+++ ProjecktMappe/src/Vpets/obj/Debug/net9.0/Vpets.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vpets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24b132c743ff1dfa128cb3cbe12926e45068a57e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84859c62b5e6ccb978fa9355cf3aeccce791d82a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vpets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vpets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
