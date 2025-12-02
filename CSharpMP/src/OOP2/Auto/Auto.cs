f17b66a6456dcd124aa4c663ababc8d2a9e52498
per5cm <per5cm@proton.me>
2025-12-02 10:08:45 +0100

    oop2 init

---
+++ CSharpMP/src/OOP2/Auto/Auto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2.Auto
+{
+    internal class Auto
+    {
+        internal string Marke { get; set; }
+        internal int Geschwindigkeit {  get; set; }
+        internal static int AnzahlAutos = 0;
+
+        internal Auto(string marke, int geschwindigkeit)
+        {
+            Marke = marke;
+            Geschwindigkeit = geschwindigkeit;
+            AnzahlAutos++;
+        }
+
+        internal void Fahren()
+        {
+            Console.WriteLine($"{Marke} Fährt mit {Geschwindigkeit + 40} km/h los!");
+        }
+        internal void Stoppen()
+        {
+            Console.WriteLine($"{Marke} hält an.");
+        }
+    }
+}
---
+++ CSharpMP/src/OOP2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OOP2.Auto;
+
+class Program
+{
+    static void Main()
+    {
+        Auto bmw = new Auto("BMW", 0);
+        Auto audi = new Auto("Audi", 0);
+
+        bmw.Fahren();
+        bmw.Stoppen();
+
+        audi.Fahren();
+        audi.Stoppen();
+
+        Console.WriteLine($"Anzahl der Autos: {Auto.AnzahlAutos}");
+    }
+}
