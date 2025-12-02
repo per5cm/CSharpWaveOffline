79a19ef42689dc943ecdd6837513d7c1989dafb3
per5cm <per5cm@proton.me>
2025-12-02 14:11:47 +0100

    boilerplates

--- CSharpMP/src/OOP3/Program.cs
+++ CSharpMP/src/OOP3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OOP3.Auto;
+
+
+namespace OOP3
+{     
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
