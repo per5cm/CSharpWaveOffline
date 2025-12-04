c1923caefd0ad3fcf3a1ac98e403b857ec0802db
per5cm <per5cm@proton.me>
2025-12-04 10:09:36 +0100

    razor init with db

---
+++ Blazor/VPets/VPets/VPets/Data/vPetsContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VPets.Models;
+
+namespace VPets.Data
+{
+    // Der DbContext ist die Verbindung zwischen C#-Klassen und der Datenbank.
+    public class VPetsContext : DbContext
+    {
+        public VPetsContext(DbContextOptions<VPetsContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Pet> Pets { get; set; }
+    }
+}
---
+++ Blazor/VPets/VPets/VPets/Models/Pet.cs
@@ -0,0 +1,11 @@
+﻿namespace VPets.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Species { get; set; }
+        public int Age { get; set; }
+        public string? OwnerName { get; set; }
+    }
+}
