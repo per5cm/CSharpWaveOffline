e4cb1079309a8d6c7660e9727bd2ac29b2ac5a3a
per5cm <per5cm@proton.me>
2025-12-08 14:23:47 +0100

    init

--- CSharpMP/src/OOP3/Program.cs
+++ CSharpMP/src/OOP3/Program.cs
@@ -10,7 +10,46 @@ namespace OOP3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Auto[] autos = { };
+            int auswahl = 0;
+
+            do
+            {
+                Console.WriteLine("Autoverwaltung");
+                Console.WriteLine("Bitte wählen Sie eine Option:");
+                Console.WriteLine("1. Anlegen Autos");
+                Console.WriteLine("2. Anzeigen Autos");
+                Console.WriteLine("3. Gesamtwert");
+                Console.WriteLine("4. Abfragen PS");
+                Console.WriteLine("5. Preise erhöhen");
+                Console.WriteLine("9. Beenden");
+
+                switch (auswahl)
+                {
+                    case 1: autos = erfassenAuto(autos); break;
+                    case 2: ausgebenAutos(autos); break;
+                    case 3: ausgabeWert(autos); break;
+                    case 4: abfragenPS(autos); break;
+                    case 5: preiseErhoehen(autos); break;
+                }
+            } while (auswahl != 9);
+        }
+
+        private static Auto[] preiseErhoehen(Auto[] autos)
+        {
+            Console.WriteLine("Prozente Erhöhung: ");
+            int prozentErhoehung = int.Parse(Console.ReadLine());
+            for (int i = 0; i < autos.Length; i++)
+            {
+                autos[i].Wert += autos[i].Wert * prozentErhoehung / 100;
+                // autos[i].Wert *= (1 + (double)prozentErhoehung / 100);
+            }
+            return autos;
+        }
+
+        private static void abfragenPS(Auto[] autos)
+        {
+
         }
     }
 }
