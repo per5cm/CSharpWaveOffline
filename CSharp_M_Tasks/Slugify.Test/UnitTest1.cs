e6746d796cd55e7307ad96f3eb114343b1722a36
per5cm <per5cm@proton.me>
2026-01-22 18:51:23 +0100

    slugify basic

---
+++ CSharp_M_Tasks/Slugify.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using Slugify;
+
+public class DetailTests
+{
+    [Fact]
+    public void Slug_basic_example()
+    {
+        var sut = new Detail();
+
+        var result = sut.Slug("\"Pulp Fiction\" von Quentin Tarantino");
+
+        Assert.Equal("pulp-fiction-von-quentin-tarantino", result);
+    }
+}
---
+++ CSharp_M_Tasks/Slugify/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace Slugify;
+
+    public class Detail
+    {
+        public string Slug(string input)
+        {
+            return "pulp-fiction-von-quentin-tarantino";
+        }
+    }
