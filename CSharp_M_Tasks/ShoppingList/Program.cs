4d522ccf194e59e06b8d23eaa40e2081a634eac0
per5cm <per5cm@proton.me>
2026-01-26 22:57:55 +0100

    shopping mini program

---
+++ CSharp_M_Tasks/ShoppingList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> shoppingList = new List<string>();
+
+        while (true)
+        {
+            Console.WriteLine("Optionen einkaufsliste zu bearbeiten: 1 = hinzufügen, 2 = entfernen, 3 = anzeigen, 4 = beenden");
+            string option = Console.ReadLine()?.ToLower().Trim() ?? "";
+            if (option == "1" || option == "hinzufügen")
+            {
+                Console.WriteLine("Artikel zum Hinzufügen eingeben: ");
+                string item = Console.ReadLine()?.ToLower().Trim() ?? "";
+                if(!string.IsNullOrWhiteSpace(item))
+                {
+                    shoppingList.Add(item);
+                    Console.WriteLine();
+                    Console.WriteLine($"\"{item}\" hinzugefügt");
+                }              
+            }
+            else if (option == "2" || option == "entfernen")
+            {
+                Console.WriteLine("Artikel zum Entfernen: ");
+                string item = Console.ReadLine()?.ToLower().Trim() ?? "";
+                Console.WriteLine();
+                if (shoppingList.Remove(item)) Console.WriteLine("Entfernt!");
+                else Console.WriteLine("Nicht Gefunden!");
+            }
+            else if (option == "3" || option == "anzeigen")
+            {
+                Console.WriteLine(string.Join(", ", shoppingList));
+            }
+            else if (option == "4" || option == "beenden") break;
+        }
+    }
+}
