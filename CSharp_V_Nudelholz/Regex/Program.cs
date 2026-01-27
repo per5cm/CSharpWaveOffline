50593818ec25465d7735df4fc59cd55b64250c41
per5cm <per5cm@proton.me>
2026-01-27 23:35:25 +0100

    new folder, regex crap

---
+++ CSharp_V_Nudelholz/Regex/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions; // <--- main player for using Regex
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Core methods for regex:
+        // Regex.IsMatch(input, pattern) -> bool match
+        // Regex.Match(input, pattern) -> match object or .Success false
+        // Regex Matches(input, pattern) -> MatchCollection -> all hits
+        // Regex.Replace(input. pattern, replacment) -> string with swaps
+        Console.WriteLine("I Love Pizza!");
+    }
+}
