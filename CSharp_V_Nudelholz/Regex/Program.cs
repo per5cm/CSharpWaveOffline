d592b52bf5d5dbcb57932626ebbb09afad024723
per5cm <per5cm@proton.me>
2026-01-28 00:51:07 +0100

    bump

--- CSharp_V_Nudelholz/Regex/Program.cs
+++ CSharp_V_Nudelholz/Regex/Program.cs
@@ -11,9 +11,63 @@ class Program
         // Regex.IsMatch(input, pattern) -> bool match
         // Regex.Match(input, pattern) -> match object or .Success false
         // Regex Matches(input, pattern) -> MatchCollection -> all hits
-        // Regex.Replace(input. pattern, replacment) -> string with swaps
+        // Regex.Replace(input, pattern, replacment) -> string with swaps
 
-        // 
-        Console.WriteLine("I Love Pizza!");
+        // Options:
+        // RegexOptions.IgnoreCase // "Hello" matches "hello"
+        // RegexOptions.Compiled // Faster for reused patterns 
+        // RegexOptions.Multiline // ^/$ match line starts/ends too
+        // RegexOptions.IgnorePatternWhitespace // allow spaces + comments in pattern
+
+        Console.WriteLine("Wery iconic Mongolian grass!");
+
+        var menuOption = new List<string>
+        {
+            "0: Exit",
+            "1: Is Valid"
+        };
+
+        while (true)
+        {
+            Console.WriteLine("!=== Regex options ===!");
+            foreach (var option in menuOption)
+                Console.WriteLine(option);
+
+            int choice = ReadInt("Auswahl: ", 0, 9);
+
+            switch (choice)
+            {
+                case 0: 
+                    Console.WriteLine("Tchussie!");
+                    return;
+            }
+        }
     }
+
+    public static bool IsValid(string value)
+    {
+        if (string.IsNullOrEmpty(value)) return false;
+
+
+
+    }
+
+    #region Helpers
+
+    static int ReadInt(string label, int min = int.MinValue, int max = int.MaxValue)
+    {
+        while (true)
+        {
+            if (min == int.MinValue && max == int.MaxValue)
+                Console.Write($"{label}: ");
+            else
+                Console.Write($"{label} ({min}-{max}): ");
+
+            if (int.TryParse(Console.ReadLine(), out int n) && n >= min && n <= max)
+                return n;
+
+            Console.WriteLine("Ungültige Eingabe.");
+        }  
+    }
+    #endregion
 }
