4baead07d025fda4e8e044b6aa2ac3bea161c63d
per5cm <per5cm@proton.me>
2026-03-18 11:03:21 +0100

    update

--- CSharp_V_Nudelholz/CellularRule/Library/Rule30.cs
+++ CSharp_V_Nudelholz/CellularRule/Library/Rule30.cs
@@ -6,6 +6,47 @@ namespace CellularRule.Library
 {
     internal class Rule30
     {
+        internal static int CellularRule30()
+        {
+            int width = 80;
 
+            int[] cellularBlankArray = new int[width];
+
+            int[] cellularFinalArray = new int[width];
+
+            int positiv = 0b0001;
+            int negativ = 0b0000;
+
+            #region UserDirection
+
+            bool end = false;
+            const ConsoleKey endButton = ConsoleKey.Q;
+
+            #endregion
+
+            cellularBlankArray[width / 2] = positiv;
+
+            int generation = 0;
+
+            while (!end)
+            {
+                Console.Clear();
+
+                Console.Write($"Generation: {0}", generation);
+
+
+
+                generation++;
+
+                var key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case endButton: end = true; break;
+                    default: break;
+                }
+            }
+
+            return width;
+        }
     }
 }
